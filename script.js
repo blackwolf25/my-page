fc30c39e7b8be36f66ee2a76443206eb7cd56745
cuongvm <heromodehp@gmail.com>
2020-04-17 20:30:24 +0700

    test

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 (function(){
-	var request = new XMLHttpRequest();
-   request.open("GET", "data.json", false);
-   request.send(null)
-   var my_JSON_object = JSON.parse(request.responseText);
-   console.log(my_JSON_object.result[0]);
+	
+
+$.getJSON("data.json", function(json) {
+    console.log(json); // this will show the info it in firebug console
+});
+
 })();
