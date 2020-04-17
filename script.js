5bd5746b32552172cb9a0b1949028b526e2ca314
cuongvm <heromodehp@gmail.com>
2020-04-17 20:53:19 +0700

    test

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 (function(){
 	
 
-$.getJSON("data.json", function(json) {
-    console.log(json); // this will show the info it in firebug console
+const posts = $.getJSON("data.json", function(json) {
+    return json;
 });
-
+console.log(posts);
 })();
